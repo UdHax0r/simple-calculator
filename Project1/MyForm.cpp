25c53cc1dd6a24c903cbe8720b43cf4f62081606
Utkarsh Dhuliya <utkarshdhuliya814@gmail.com>
2024-05-02 15:49:13 +0530

    Initial commit

--- Project1/MyForm.cpp
+++ Project1/MyForm.cpp
@@ -6,7 +6,7 @@ using namespace System;
 using namespace System::Windows::Forms;
 [STAThreadAttribute]
 
-void main(array<String^>^ args) {
+void main(array<String^>^ args){
 	Application::SetCompatibleTextRenderingDefault(false);
 	Application::EnableVisualStyles();
 	Project1::MyForm form;
